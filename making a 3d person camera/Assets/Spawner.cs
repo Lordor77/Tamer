f65438051a8f601f2fade72cce6b7577b98b7849
InonAngel <inon.angel412@gmail.com>
2018-01-04 17:08:14 +0200

    Spawner is in the house
    Spawner is in the house

---
+++ making a 3d person camera/Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Monster;
+    private float RepeatTime = 3f;
+    int i = 0;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2f, RepeatTime);
+    }
+
+    void Spawn()
+    {
+        if (i < 5)
+        {
+            Instantiate(Monster, transform.position, Quaternion.identity);
+            i++;
+        }
+    }
+
+}
