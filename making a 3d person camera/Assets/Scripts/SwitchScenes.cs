2af94137eaf971e70f0512f50ce854e8fb2dc807
Dor Dahan <lordor777@gmail.com>
2018-01-04 23:55:12 +0200

    Battle Scene
    Battle Scene and monster movement.

---
+++ making a 3d person camera/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+
+    private string scene = "BattleScene";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tamer"))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
