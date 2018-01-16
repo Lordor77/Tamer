3d3062470cf1e1f2a0859d8a2970acf4559ebb40
Dor Dahan <lordor777@gmail.com>
2018-01-16 09:20:29 +0200

    Tasks 16-19
    Plus the beggining of task 20.

---
+++ making a 3d person camera/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Variables
+    [SerializeField]
+    public Stat health;
+    [SerializeField]
+    public Stat energy;
+    bool energyNeedRegen;
+    float energyRegenTimer;
+
+    [SerializeField]
+    public GameObject enemyMonsterCam;
+
+
+    // Use this for initialization
+    private void Awake ()
+    {
+        health.Initialize();
+        energy.Initialize();
+
+        energyNeedRegen = false;
+        energyRegenTimer = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Energy Regeneration shit
+        if (this.gameObject.GetComponent<Player>().energy.CurrentVal < 100)
+            energyNeedRegen = true;
+        else
+        {
+            energyNeedRegen = false;
+            energyRegenTimer = 1;
+        }
+
+        if (energyNeedRegen)
+            energyRegenTimer -= Time.deltaTime;
+
+        if (energyRegenTimer <= 0)
+        {
+            this.gameObject.GetComponent<Player>().energy.CurrentVal += 1;
+            energyRegenTimer = 1;
+        }
+
+        // Capture the enemy monster
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            enemyMonsterCam.SetActive(true);
+        }
+
+
+        /* TESTING
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            health.CurrentVal -= 10;
+            energy.CurrentVal -= 10;
+        }
+        */
+    }
+}
--- making a 3d person camera/Assets/Scripts/Player/PlayerShoot.cs
+++ making a 3d person camera/Assets/Scripts/Player/PlayerShoot.cs
@@ -7,23 +7,38 @@ public class PlayerShoot : MonoBehaviour {
     // Variables
     [SerializeField]
     GameObject Bullet;
+    float attackCoolDown;
+    float energyCost;
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start () 
+    {
+        attackCoolDown = 0;
+        energyCost = 10;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.E)) {
+	void Update () 
+    {
+        if (attackCoolDown > 0)
+            attackCoolDown -= Time.deltaTime;
+        
+		if (this.gameObject.GetComponent<Player>().energy.CurrentVal >= energyCost && Input.GetKeyDown(KeyCode.E) && attackCoolDown <= 0) {
             // Create the Bullet from the Bullet Prefab
             var bullet = (GameObject)Instantiate(Bullet, this.transform.position, this.transform.rotation);
 
             // Add velocity to the bullet
             bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 12;
 
+            // Reducing the energy value
+            this.gameObject.GetComponent<Player>().energy.CurrentVal -= energyCost;
+
             // Destroy the bullet after 2 seconds
             Destroy(bullet, 20.0f);
+
+            // Resets this attack's cool down
+            attackCoolDown = 2.5f;
         }
 	}
 }
